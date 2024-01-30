54ffcf999f6fa9b760da99c143e52823cd3677a1
ChiragAgarwal999 <117121912+ChiragAgarwal999@users.noreply.github.com>
2024-01-31 04:27:56 +0530

    Update app.js
    change root /

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
 
 //Index Route
 app.get("/listings", async (req, res) => {
